4033f15c57f311e7479b6dbe742c78049636f359
mbalzano19 <125160521+mbalzano19@users.noreply.github.com>
2023-05-31 13:32:31 -0400

    part 3

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+
 
 
 # Create your views here.
@@ -21,3 +23,15 @@ def finches_detail(request, finch_id):
     return render(request, 'finches/detail.html', {
         'finch': finch
     })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches'
